1c6bbafe99ea693d1bffbee810ec7c55a6bf8a44
Mathis <lb.brangeon@gmail.com>
2023-03-02 16:20:38 +0100

    v0.1.0
    - sidebar

--- components/sidebar.php
+++ components/sidebar.php
@@ -4,7 +4,7 @@ $activePage = basename($_SERVER['PHP_SELF'], ".php");
 
 <div class="d-flex flex-column bg-sidebard h-100 justify-content-between nav" style="width: 280px; padding: 32px 25px 25px 0;">
     <div class="d-flex flex-column">
-        <a href="/" class="d-flex justify-content-center align-items-center">
+        <a href="./dashboard.php" class="d-flex justify-content-center align-items-center">
             <img src="./assets/img/logo.svg" alt="logo">
         </a>
         <hr />
@@ -18,7 +18,7 @@ $activePage = basename($_SERVER['PHP_SELF'], ".php");
                 </a>
             </li>
             <li class="<?= ($activePage == 'activity') ? 'activeNav' : ''; ?> d-flex justify-content-start">
-                <a href=" #" class="nav-link fw-semibold d-flex align-items-center gap-3 ps-4 py-3">
+                <a href="./activity.php" class="nav-link fw-semibold d-flex align-items-center gap-3 ps-4 py-3">
                     <svg xmlns="http://www.w3.org/2000/svg" width="16" height="16" fill="currentColor" class="bi bi-card-checklist" viewBox="0 0 16 16">
                         <path d="M14.5 3a.5.5 0 0 1 .5.5v9a.5.5 0 0 1-.5.5h-13a.5.5 0 0 1-.5-.5v-9a.5.5 0 0 1 .5-.5h13zm-13-1A1.5 1.5 0 0 0 0 3.5v9A1.5 1.5 0 0 0 1.5 14h13a1.5 1.5 0 0 0 1.5-1.5v-9A1.5 1.5 0 0 0 14.5 2h-13z" />
                         <path d="M7 5.5a.5.5 0 0 1 .5-.5h5a.5.5 0 0 1 0 1h-5a.5.5 0 0 1-.5-.5zm-1.496-.854a.5.5 0 0 1 0 .708l-1.5 1.5a.5.5 0 0 1-.708 0l-.5-.5a.5.5 0 1 1 .708-.708l.146.147 1.146-1.147a.5.5 0 0 1 .708 0zM7 9.5a.5.5 0 0 1 .5-.5h5a.5.5 0 0 1 0 1h-5a.5.5 0 0 1-.5-.5zm-1.496-.854a.5.5 0 0 1 0 .708l-1.5 1.5a.5.5 0 0 1-.708 0l-.5-.5a.5.5 0 0 1 .708-.708l.146.147 1.146-1.147a.5.5 0 0 1 .708 0z" />
@@ -30,12 +30,12 @@ $activePage = basename($_SERVER['PHP_SELF'], ".php");
     </div>
     <div>
         <hr />
-        <div class="d-flex align-items-center justify-content-center">
+        <a class="d-flex align-items-center justify-content-center gap-2 logout cursor-pointer" href="logout.php">
             <svg xmlns="http://www.w3.org/2000/svg" width="16" height="16" fill="currentColor" class="bi bi-box-arrow-left" viewBox="0 0 16 16">
                 <path fill-rule="evenodd" d="M6 12.5a.5.5 0 0 0 .5.5h8a.5.5 0 0 0 .5-.5v-9a.5.5 0 0 0-.5-.5h-8a.5.5 0 0 0-.5.5v2a.5.5 0 0 1-1 0v-2A1.5 1.5 0 0 1 6.5 2h8A1.5 1.5 0 0 1 16 3.5v9a1.5 1.5 0 0 1-1.5 1.5h-8A1.5 1.5 0 0 1 5 12.5v-2a.5.5 0 0 1 1 0v2z" />
                 <path fill-rule="evenodd" d="M.146 8.354a.5.5 0 0 1 0-.708l3-3a.5.5 0 1 1 .708.708L1.707 7.5H10.5a.5.5 0 0 1 0 1H1.707l2.147 2.146a.5.5 0 0 1-.708.708l-3-3z" />
             </svg>
             Se deconnecter
-        </div>
+        </a>
     </div>
 </div>
