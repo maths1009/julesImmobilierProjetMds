3bf49295c46d4fcb7272959b6177f46391305b21
Mathis <lb.brangeon@gmail.com>
2023-03-02 16:38:46 +0100

    fix

--- components/sidebar.php
+++ components/sidebar.php
@@ -4,13 +4,13 @@ $activePage = basename($_SERVER['PHP_SELF'], ".php");
 
 <div class="d-flex flex-column bg-sidebard h-100 justify-content-between nav" style="width: 280px; padding: 32px 25px 25px 0;">
     <div class="d-flex flex-column">
-        <a href="./dashboard.php" class="d-flex justify-content-center align-items-center">
+        <a href="./index.php" class="d-flex justify-content-center align-items-center">
             <img src="./assets/img/logo.svg" alt="logo">
         </a>
         <hr />
         <ul class="d-flex flex-column gap-5 p-0">
             <li class="<?= ($activePage == 'index') ? 'activeNav' : ''; ?> d-flex justify-content-start">
-                <a href="#" class="nav-link fw-semibold d-flex align-items-center gap-3 justify-content-start ps-4 py-3">
+                <a href="./index.php" class="nav-link fw-semibold d-flex align-items-center gap-3 justify-content-start ps-4 py-3">
                     <svg xmlns="http://www.w3.org/2000/svg" width="16" height="16" fill="currentColor" class="bi bi-house" viewBox="0 0 16 16">
                         <path d="M8.707 1.5a1 1 0 0 0-1.414 0L.646 8.146a.5.5 0 0 0 .708.708L2 8.207V13.5A1.5 1.5 0 0 0 3.5 15h9a1.5 1.5 0 0 0 1.5-1.5V8.207l.646.647a.5.5 0 0 0 .708-.708L13 5.793V2.5a.5.5 0 0 0-.5-.5h-1a.5.5 0 0 0-.5.5v1.293L8.707 1.5ZM13 7.207V13.5a.5.5 0 0 1-.5.5h-9a.5.5 0 0 1-.5-.5V7.207l5-5 5 5Z" />
                     </svg>
