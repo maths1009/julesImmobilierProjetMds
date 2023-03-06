4820efbba98d456c519b4ff7dda985910b6ef484
Mathis <lb.brangeon@gmail.com>
2023-03-06 14:33:29 +0100

    Fix

--- index.php
+++ index.php
@@ -5,10 +5,10 @@ include './components/head.php';
 <body>
   <div class="d-flex flex-row w-100 vh-100">
     <?php require './components/sidebar.php' ?>
-    <main class="w-100">
-        <?php require './components/header.php' ?>
+    <div class="w-100 m-5">
+      <?php require './components/header.php' ?>
       <a href="login.php">login</a>
-    </main>
+    </div>
   </div>
 </body>
 
