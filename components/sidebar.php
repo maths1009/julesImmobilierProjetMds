00d7e757e540749c3f448c78385cb898ca0c5755
Tom <cartier.tom49@gmail.com>
2023-03-06 14:20:57 +0100

    Save

--- components/sidebar.php
+++ components/sidebar.php
@@ -1,4 +1,5 @@
 <?php
+include_once('./config.php');
 $activePage = basename($_SERVER['PHP_SELF'], ".php");
 ?>
 
@@ -26,6 +27,22 @@ $activePage = basename($_SERVER['PHP_SELF'], ".php");
                     Activités
                 </a>
             </li>
+            <?php
+            if ($_SESSION['user']['role_id'] == 1) {
+            ?>
+                <li class="<?= ($activePage == 'agents') ? 'activeNav' : ''; ?> d-flex justify-content-start">
+                    <a href="./agents.php" class="nav-link fw-semibold d-flex align-items-center gap-3 ps-4 py-3">
+                        <svg xmlns="http://www.w3.org/2000/svg" width="16" height="16" fill="currentColor" class="bi bi-card-checklist" viewBox="0 0 16 16">
+                            <path d="M14.5 3a.5.5 0 0 1 .5.5v9a.5.5 0 0 1-.5.5h-13a.5.5 0 0 1-.5-.5v-9a.5.5 0 0 1 .5-.5h13zm-13-1A1.5 1.5 0 0 0 0 3.5v9A1.5 1.5 0 0 0 1.5 14h13a1.5 1.5 0 0 0 1.5-1.5v-9A1.5 1.5 0 0 0 14.5 2h-13z" />
+                            <path d="M7 5.5a.5.5 0 0 1 .5-.5h5a.5.5 0 0 1 0 1h-5a.5.5 0 0 1-.5-.5zm-1.496-.854a.5.5 0 0 1 0 .708l-1.5 1.5a.5.5 0 0 1-.708 0l-.5-.5a.5.5 0 1 1 .708-.708l.146.147 1.146-1.147a.5.5 0 0 1 .708 0zM7 9.5a.5.5 0 0 1 .5-.5h5a.5.5 0 0 1 0 1h-5a.5.5 0 0 1-.5-.5zm-1.496-.854a.5.5 0 0 1 0 .708l-1.5 1.5a.5.5 0 0 1-.708 0l-.5-.5a.5.5 0 0 1 .708-.708l.146.147 1.146-1.147a.5.5 0 0 1 .708 0z" />
+                        </svg>
+                        Mes agents
+                    </a>
+                </li>
+            <?php
+            }
+            ?>
+
         </ul>
     </div>
     <div>
--- login.php
+++ login.php
@@ -1,9 +1,7 @@
 <?php
-session_start();
-include_once("./classes/dd.php");
+include_once("./config.php");
 // include_once("../classes/dbGestion.php");
 // $connect = new dbGestion("julesimmo", "users");
-$debug = new DdService();
 $mysqli = new mysqli("localhost", "root", "root", "julesimmo");
 
 // Get user connect
@@ -19,6 +17,7 @@ if ($_SERVER["REQUEST_METHOD"] == "POST" && isset($_POST['login'])) {
     $result = $stmt->get_result();
     $verif = $result->fetch_assoc();
     if (isset($verif)) {
+        $_SESSION['user'] = $verif;
         header('Location: http://localhost:8888/julesImmobilierProjetMds/index.php');
     } else {
         $_SESSION['status'] = "L'email ou le mot de passe est invalide !";
@@ -79,52 +78,3 @@ if ($_SERVER["REQUEST_METHOD"] == "POST" && isset($_POST['login'])) {
         </form>
     </main>
 </body>
-<<<<<<< HEAD
-<!DOCTYPE html>
-<html lang="fr">
-
-<head>
-    <meta charset="UTF-8">
-    <meta http-equiv="X-UA-Compatible" content="IE=edge">
-    <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>Document</title>
-    <link rel="stylesheet" href="./assets/css/normalizeCSS.css" />
-    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.0-alpha1/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-GLhlTQ8iRABdZLl6O3oVMWSktQOp6b7In1Zl3/Jr59b6EGGoI1aFkw7cmDA6j6gD" crossorigin="anonymous" />
-    <link rel="stylesheet" href="./assets/css/styles.css" />
-</head>
-
-<body>
-    <main>
-        <form class="vh-100 bg-primary">
-            <div class="container py-5 h-100">
-                <div class="row d-flex justify-content-center align-items-center h-100">
-                    <div class="col-12 col-md-8 col-lg-6 col-xl-5">
-                        <div class="card shadow-2-strong" style="border-radius: 1rem;">
-                            <div class="card-body p-5 text-center">
-
-                                <h3 class="mb-5">Bienvenu chez Jules Immo</h3>
-
-                                <div class="form-outline mb-4">
-                                    <input type="email" id="typeEmailX-2" class="form-control form-control-lg" />
-                                    <label class="form-label" for="typeEmailX-2">Email</label>
-                                </div>
-
-                                <div class="form-outline mb-4">
-                                    <input type="password" id="typePasswordX-2" class="form-control form-control-lg" />
-                                    <label class="form-label" for="typePasswordX-2">Mot de passe</label>
-                                </div>
-
-                                <button class="btn btn-primary btn-lg btn-block" type="submit">connexion</button>
-
-                            </div>
-                        </div>
-                    </div>
-                </div>
-            </div>
-        </form>
-    </main>
-</body>
-=======
->>>>>>> Tom
-
-</html>
