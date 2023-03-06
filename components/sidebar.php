dc28512988aaddf3e685f78b6c25dad9bed1b6d6
Mathis <lb.brangeon@gmail.com>
2023-03-06 10:00:26 +0100

    test

--- components/sidebar.php
+++ components/sidebar.php
@@ -1,3 +1,41 @@
+<?php
+$activePage = basename($_SERVER['PHP_SELF'], ".php");
+?>
 
-<?php$activePage = basename($_SERVER['PHP_SELF'], ".php");?><div class="d-flex flex-column bg-sidebard h-100 justify-content-between nav" style="width: 280px; padding: 32px 25px 25px 0;"><div class="d-flex flex-column"><a href="./index.php" class="d-flex justify-content-center align-items-center"><img src="./assets/img/logo.svg" alt="logo"></a><hr /><ul class="d-flex flex-column gap-5 p-0"><li class="<?= ($activePage == 'index') ? 'activeNav' : ''; ?> d-flex justify-content-start"><a href="./index.php" class="nav-link fw-semibold d-flex align-items-center gap-3 justify-content-start ps-4 py-3"><svg xmlns="http://www.w3.org/2000/svg" width="16" height="16" fill="currentColor" class="bi bi-house" viewBox="0 0 16 16"><path d="M8.707 1.5a1 1 0 0 0-1.414 0L.646 8.146a.5.5 0 0 0 .708.708L2 8.207V13.5A1.5 1.5 0 0 0 3.5 15h9a1.5 1.5 0 0 0 1.5-1.5V8.207l.646.647a.5.5 0 0 0 .708-.708L13 5.793V2.5a.5.5 0 0 0-.5-.5h-1a.5.5 0 0 0-.5.5v1.293L8.707 1.5ZM13 7.207V13.5a.5.5 0 0 1-.5.5h-9a.5.5 0 0 1-.5-.5V7.207l5-5 5 5Z" /></svg>Dashboard</a></li><li class="<?= ($activePage == 'activity') ? 'activeNav' : ''; ?> d-flex justify-content-start"><a href="./activity.php" class="nav-link fw-semibold d-flex align-items-center gap-3 ps-4 py-3"><svg xmlns="http://www.w3.org/2000/svg" width="16" height="16" fill="currentColor" class="bi bi-card-checklist" viewBox="0 0 16 16"><path d="M14.5 3a.5.5 0 0 1 .5.5v9a.5.5 0 0 1-.5.5h-13a.5.5 0 0 1-.5-.5v-9a.5.5 0 0 1 .5-.5h13zm-13-1A1.5 1.5 0 0 0 0 3.5v9A1.5 1.5 0 0 0 1.5 14h13a1.5 1.5 0 0 0 1.5-1.5v-9A1.5 1.5 0 0 0 14.5 2h-13z" /><path d="M7 5.5a.5.5 0 0 1 .5-.5h5a.5.5 0 0 1 0 1h-5a.5.5 0 0 1-.5-.5zm-1.496-.854a.5.5 0 0 1 0 .708l-1.5 1.5a.5.5 0 0 1-.708 0l-.5-.5a.5.5 0 1 1 .708-.708l.146.147 1.146-1.147a.5.5 0 0 1 .708 0zM7 9.5a.5.5 0 0 1 .5-.5h5a.5.5 0 0 1 0 1h-5a.5.5 0 0 1-.5-.5zm-1.496-.854a.5.5 0 0 1 0 .708l-1.5 1.5a.5.5 0 0 1-.708 0l-.5-.5a.5.5 0 0 1 .708-.708l.146.147 1.146-1.147a.5.5 0 0 1 .708 0z" /></svg>Activités</a></li></ul></div><div><hr /><a class="d-flex align-items-center justify-content-center gap-2 logout cursor-pointer" href="logout.php"><svg xmlns="http://www.w3.org/2000/svg" width="16" height="16" fill="currentColor" class="bi bi-box-arrow-left" viewBox="0 0 16 16"><path fill-rule="evenodd" d="M6 12.5a.5.5 0 0 0 .5.5h8a.5.5 0 0 0 .5-.5v-9a.5.5 0 0 0-.5-.5h-8a.5.5 0 0 0-.5.5v2a.5.5 0 0 1-1 0v-2A1.5 1.5 0 0 1 6.5 2h8A1.5 1.5 0 0 1 16 3.5v9a1.5 1.5 0 0 1-1.5 1.5h-8A1.5 1.5 0 0 1 5 12.5v-2a.5.5 0 0 1 1 0v2z" /><path fill-rule="evenodd" d="M.146 8.354a.5.5 0 0 1 0-.708l3-3a.5.5 0 1 1 .708.708L1.707 7.5H10.5a.5.5 0 0 1 0 1H1.707l2.147 2.146a.5.5 0 0 1-.708.708l-3-3z" /></svg>Se deconnecter</a></div></div>
-
+<div class="d-flex flex-column bg-sidebard h-100 justify-content-between nav" style="width: 280px; padding: 32px 25px 25px 0;">
+    <div class="d-flex flex-column">
+        <a href="./index.php" class="d-flex justify-content-center align-items-center">
+            <img src="./assets/img/logo.svg" alt="logo">
+        </a>
+        <hr />
+        <ul class="d-flex flex-column gap-5 p-0">
+            <li class="<?= ($activePage == 'index') ? 'activeNav' : ''; ?> d-flex justify-content-start">
+                <a href="./index.php" class="nav-link fw-semibold d-flex align-items-center gap-3 justify-content-start ps-4 py-3">
+                    <svg xmlns="http://www.w3.org/2000/svg" width="16" height="16" fill="currentColor" class="bi bi-house" viewBox="0 0 16 16">
+                        <path d="M8.707 1.5a1 1 0 0 0-1.414 0L.646 8.146a.5.5 0 0 0 .708.708L2 8.207V13.5A1.5 1.5 0 0 0 3.5 15h9a1.5 1.5 0 0 0 1.5-1.5V8.207l.646.647a.5.5 0 0 0 .708-.708L13 5.793V2.5a.5.5 0 0 0-.5-.5h-1a.5.5 0 0 0-.5.5v1.293L8.707 1.5ZM13 7.207V13.5a.5.5 0 0 1-.5.5h-9a.5.5 0 0 1-.5-.5V7.207l5-5 5 5Z" />
+                    </svg>
+                    Dashboard
+                </a>
+            </li>
+            <li class="<?= ($activePage == 'activity') ? 'activeNav' : ''; ?> d-flex justify-content-start">
+                <a href="./activity.php" class="nav-link fw-semibold d-flex align-items-center gap-3 ps-4 py-3">
+                    <svg xmlns="http://www.w3.org/2000/svg" width="16" height="16" fill="currentColor" class="bi bi-card-checklist" viewBox="0 0 16 16">
+                        <path d="M14.5 3a.5.5 0 0 1 .5.5v9a.5.5 0 0 1-.5.5h-13a.5.5 0 0 1-.5-.5v-9a.5.5 0 0 1 .5-.5h13zm-13-1A1.5 1.5 0 0 0 0 3.5v9A1.5 1.5 0 0 0 1.5 14h13a1.5 1.5 0 0 0 1.5-1.5v-9A1.5 1.5 0 0 0 14.5 2h-13z" />
+                        <path d="M7 5.5a.5.5 0 0 1 .5-.5h5a.5.5 0 0 1 0 1h-5a.5.5 0 0 1-.5-.5zm-1.496-.854a.5.5 0 0 1 0 .708l-1.5 1.5a.5.5 0 0 1-.708 0l-.5-.5a.5.5 0 1 1 .708-.708l.146.147 1.146-1.147a.5.5 0 0 1 .708 0zM7 9.5a.5.5 0 0 1 .5-.5h5a.5.5 0 0 1 0 1h-5a.5.5 0 0 1-.5-.5zm-1.496-.854a.5.5 0 0 1 0 .708l-1.5 1.5a.5.5 0 0 1-.708 0l-.5-.5a.5.5 0 0 1 .708-.708l.146.147 1.146-1.147a.5.5 0 0 1 .708 0z" />
+                    </svg>
+                    Activités
+                </a>
+            </li>
+        </ul>
+    </div>
+    <div>
+        <hr />
+        <a class="d-flex align-items-center justify-content-center gap-2 logout cursor-pointer" href="logout.php">
+            <svg xmlns="http://www.w3.org/2000/svg" width="16" height="16" fill="currentColor" class="bi bi-box-arrow-left" viewBox="0 0 16 16">
+                <path fill-rule="evenodd" d="M6 12.5a.5.5 0 0 0 .5.5h8a.5.5 0 0 0 .5-.5v-9a.5.5 0 0 0-.5-.5h-8a.5.5 0 0 0-.5.5v2a.5.5 0 0 1-1 0v-2A1.5 1.5 0 0 1 6.5 2h8A1.5 1.5 0 0 1 16 3.5v9a1.5 1.5 0 0 1-1.5 1.5h-8A1.5 1.5 0 0 1 5 12.5v-2a.5.5 0 0 1 1 0v2z" />
+                <path fill-rule="evenodd" d="M.146 8.354a.5.5 0 0 1 0-.708l3-3a.5.5 0 1 1 .708.708L1.707 7.5H10.5a.5.5 0 0 1 0 1H1.707l2.147 2.146a.5.5 0 0 1-.708.708l-3-3z" />
+            </svg>
+            Se deconnecter
+        </a>
+    </div>
+</div>
--- login.php
+++ login.php
@@ -1,85 +1,3 @@
-<?php
-session_start();
-include_once("./classes/dd.php");
-// include_once("../classes/dbGestion.php");
-// $connect = new dbGestion("julesimmo", "users");
-$debug = new DdService();
-$mysqli = new mysqli("localhost", "root", "root", "julesimmo");
-
-// Get user connect
-if ($_SERVER["REQUEST_METHOD"] == "POST" && isset($_POST['login'])) {
-    $email = $_POST["email"];
-    $password = hash('sha256', $_POST["password"]);
-
-    $stmt = $mysqli->prepare('SELECT * FROM users WHERE email = ? AND password = ?');
-    $stmt->bind_param('ss', $email, $password);
-
-    $stmt->execute();
-
-    $result = $stmt->get_result();
-    $verif = $result->fetch_assoc();
-    if (isset($verif)) {
-        header('Location: http://localhost:8888/julesImmobilierProjetMds/index.php');
-    } else {
-        $_SESSION['status'] = "L'email ou le mot de passe est invalide !";
-    }
-}
-?>
-
-<!DOCTYPE html>
-<html lang="fr">
-
-<head>
-    <meta charset="UTF-8">
-    <meta http-equiv="X-UA-Compatible" content="IE=edge">
-    <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>Document</title>
-    <link rel="stylesheet" href="./assets/css/normalizeCSS.css" />
-    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.0-alpha1/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-GLhlTQ8iRABdZLl6O3oVMWSktQOp6b7In1Zl3/Jr59b6EGGoI1aFkw7cmDA6j6gD" crossorigin="anonymous" />
-    <link rel="stylesheet" href="./assets/css/styles.css" />
-</head>
-
-<body>
-    <main class="bg-login">
-        <form class="vh-100" action="" method="POST">
-
-            <?php if (isset($_SESSION['status'])) { ?>
-                <div class="alert alert-danger position-fixed start-50 translate-middle-x mt-5" role="alert">
-                    <?php echo ($_SESSION['status']); ?>
-                </div>
-            <?php unset($_SESSION['status']);
-            } ?>
-
-            <div class="container h-100 primary">
-                <div class="row d-flex justify-content-center align-items-center h-100 flex-column gap-5">
-                    <img class="mw-30" src="./assets/images/logo.svg" alt="">
-                    <div class="col-12 col-md-8 col-lg-6 col-xl-5">
-                        <div class="card shadow-2-strong" style="border-radius: 1rem;">
-                            <div class="card-body p-5 text-center">
-
-                                <h3 class="mb-5 fs-2">Bienvenue chez Jules Immobilier</h3>
-
-                                <div class="form-outline mb-4 d-flex flex-column align-items-start fs-4">
-                                    <label class="form-label" for="email">Email</label>
-                                    <input type="email" name="email" id="email" class="form-control form-control-lg" />
-                                </div>
-
-                                <div class="form-outline mb-4 d-flex flex-column align-items-start fs-4">
-                                    <label class="form-label" for="password">Mot de passe</label>
-                                    <input type="password" name="password" id="password" class="form-control form-control-lg" />
-                                </div>
-
-                                <button class="btn btn-primary btn-lg btn-block" type="submit" name="login">connexion</button>
-
-                            </div>
-                        </div>
-                    </div>
-                </div>
-            </div>
-        </form>
-    </main>
-</body>
-<<<<<<< HEAD
 <!DOCTYPE html>
 <html lang="fr">
 
@@ -124,7 +42,5 @@ if ($_SERVER["REQUEST_METHOD"] == "POST" && isset($_POST['login'])) {
         </form>
     </main>
 </body>
-=======
->>>>>>> Tom
 
 </html>
