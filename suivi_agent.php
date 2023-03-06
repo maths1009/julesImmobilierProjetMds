951cd7da60b828104142c3fdab030ffc253d28e4
Maxime Lemesle <maxime.lemesle7@gmail.com>
2023-03-06 11:54:49 +0100

    Add page suivi_agent

---
+++ suivi_agent.php
@@ -0,0 +1,20 @@
+<?php
+include './components/head.php';
+?>
+
+<body>
+  <div class="d-flex flex-row w-100 vh-100">
+    <?php require './components/sidebar.php' ?>
+    <main>
+
+    <div class="title">
+        <h2 class="d-flex flex-row gap-3 fs-1 text-primary">Suivi des agents immobiliers</h2>
+        <h3>Agent immobilier</h3>
+    </div>
+
+
+    </main>
+  </div>
+</body>
+
+</html>
